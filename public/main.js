2c45c9e087b72d0d3c0362ac9bd959aa46424019
boonaki <joshdurr17@gmail.com>
2022-06-11 21:44:05 -0400

    added app files

---
+++ public/main.js
@@ -0,0 +1,78 @@
+let currentDay = document.querySelector('#currentDay')
+currentDay.addEventListener('click', returnToPresent)
+document.querySelector('#random').addEventListener('click', getRandom)
+document.querySelector('#button').addEventListener('click', getFetch)
+
+//find today and store in variable
+let today = new Date();
+let DD = String(today.getDate()).padStart(2, '0');
+let MM = String(today.getMonth() + 1).padStart(2, '0');
+let YYYY = today.getFullYear();
+today = YYYY + '-' + MM + '-' + DD;
+
+//on page load, display data for the current day
+window.onload = getFetch()
+
+function getFetch(){
+    let choice = document.querySelector('input').value
+    if(!choice){
+        choice = today
+    }
+    if(choice !== today){
+        currentDay.style.display = 'block'
+    }
+    fetch(`/pic/${choice}`)
+        .then(res => res.json())
+        .then(data => {
+        console.log(data)
+        if( data.mediatype === 'image' ){
+            document.querySelector('img').src = data.hdurl
+        }else if(data.mediatype === 'video'){
+            document.querySelector('iframe').src = data.video
+        }
+        document.querySelector('h3').innerText = data.explan
+        document.querySelector('#title').innerText = data.title
+    })
+}
+
+//displays data for present day
+function returnToPresent(){
+    fetch(`/pic/${today}`)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        currentDay.style.display = 'none'
+        if( data.mediatype === 'image' ){
+          document.querySelector('img').src = data.hdurl
+          document.querySelector('iframe').classList.add('hidden')
+
+        }else if(data.mediatype === 'video'){
+          document.querySelector('iframe').classList.remove('hidden')
+          document.querySelector('iframe').src = data.video
+        }
+        document.querySelector('h3').innerText = data.explan
+        document.querySelector('#title').innerText = data.title
+        })
+        .catch(err => console.error(err))
+}
+
+//displays data for a random day
+function getRandom(){
+  fetch('/random')
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        currentDay.style.display = 'block'
+        console.log(data)
+            if(data.mediatype=== 'image'){
+                document.querySelector('img').src = data.hdurl
+                document.querySelector('iframe').classList.add('hidden') 
+            }else if(data.mediatype === 'video'){
+                document.querySelector('iframe').classList.remove('hidden')
+                document.querySelector('iframe').src = data.video
+            }
+            document.querySelector('h3').innerText = data.explan
+            document.querySelector('#title').innerText = data.title
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+app.use(express.static('public'))
+
+//shows index.html
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+//finds specified date and sends obj
+app.get('/pic/:date', (req,res) => {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.KEY}&date=${req.params.date}`
+    fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        res.send({ mediatype: data.media_type, hdurl: data.hdurl, video: data.url, explan: data.explanation, title: data.title})
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+})
+
+//finds random date and sends obj
+app.get('/random', (req,res) => {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.KEY}&count=1`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            res.send({ mediatype: data[0].media_type, hdurl: data[0].hdurl, video: data[0].url, explan: data[0].explanation, title: data[0].title })
+        })
+        .catch(error => console.error(error))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is running`)
+})
